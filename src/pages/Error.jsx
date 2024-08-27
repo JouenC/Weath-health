dc6e6cd6bc383170f012e7fe9d6aa49b189285c4
Charles <charles.jouenne@gmail.com>
2024-08-27 17:54:40 +0200

    Mise en place des premières pages

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+    return (
+        <main aria-labelledby="page-title">
+        <h2 tabIndex="0" id="page-title" className="error-msg">
+          Page not found
+        </h2>
+      </main>
+    );
+};
+
+export default Error;
