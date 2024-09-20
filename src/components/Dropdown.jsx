3eed0965ccdf90925c3472ccc162834eed84f276
Charles <charles.jouenne@gmail.com>
2024-09-20 06:02:30 +0200

    Changement d'un className

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -7,22 +7,34 @@ import PropTypes from 'prop-types';
  * @param   {string}      props.className        [input wrapper className]
  * @param   {string}      props.id               [label "html for" identifiant]
  * @param   {string}      props.label            [label name]
- * @param   {object}      props.select           [select data for mapping]
+ * @param   {Array}       props.select           [select data for mapping]
  * @param   {function}    props.handleChange     [handling input change]
  *
  * @returns {Reactnode}   jsx injected in DOM
  */
- export default function Dropdown({
+export default function Dropdown({
   className,
   id,
   label,
   select,
   handleChange,
-})
+}) {
+  
+  // Fonction pour rendre les options
+  const renderOptions = () =>
+    select.map((item) => (
+      <option
+        title="dropdownOption"
+        type="text"
+        value={item.value}
+        key={item.abbrev || item.value} // Clé de secours si `abbrev` n'existe pas
+      >
+        {item.label}
+      </option>
+    ));
 
-{
   return (
-    <div className={`form-newEmployee--inputWrapper ${className}`}>
+    <div className={`form--inputWrapper ${className}`}>
       <label htmlFor={id}>{label}</label>
       <select
         className="dropdownList"
@@ -30,17 +42,9 @@ import PropTypes from 'prop-types';
         onChange={handleChange}
         aria-required="true"
         required
+        defaultValue="" // Ajout d'une option par défaut vide
       >
-        {select.map((item) => (
-          <option
-            title="dropdownOption"
-            type="text"
-            value={item.value}
-            key={item.abbrev}
-          >
-            {item.label}
-          </option>
-        ))}
+        {renderOptions()}
       </select>
     </div>
   );
@@ -52,7 +56,18 @@ import PropTypes from 'prop-types';
 Dropdown.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
-  select: PropTypes.array.isRequired,
+  className: PropTypes.string,
+  select: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      label: PropTypes.string.isRequired,
+      abbrev: PropTypes.string, // `abbrev` est facultatif
+    })
+  ).isRequired,
   handleChange: PropTypes.func.isRequired,
+};
+
+// Default for `className`
+Dropdown.defaultProps = {
+  className: '', 
 };
